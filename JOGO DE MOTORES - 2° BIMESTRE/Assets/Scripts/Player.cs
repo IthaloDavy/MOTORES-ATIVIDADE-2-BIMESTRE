9817bf55878923127a4730cf1b32cb9d9e176d7a
IthaloDavy <129884701+IthaloDavy@users.noreply.github.com>
2024-11-21 16:57:27 -0300

    jogo completo
    animações do player, animator e mecânicas básicas de física.

--- JOGO DE MOTORES - 2° BIMESTRE/Assets/Scripts/Player.cs
+++ JOGO DE MOTORES - 2° BIMESTRE/Assets/Scripts/Player.cs
@@ -1,25 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Player : MonoBehaviour
 {
+    public float JumpForce;
     public float Speed;
 
-    public float JumpForce;
+    public bool isJumping;
+    public bool DoubleJump;
 
     private Rigidbody2D rig;
-
     private Animator anim;
-
-    public bool isJumping;
-
-    public bool doubleJump;
-
-    // Start is called before the first frame update
+    
+   
     void Start()
     {
-        rig = GetComponent<Rigidbody2D>();
+        rig = GetComponent<Rigidbody2D>(); 
         anim = GetComponent<Animator>();
     }
 
@@ -35,59 +33,68 @@ public class Player : MonoBehaviour
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
 
-        if (Input.GetAxis("Horizontal") > 0f)
+        if((Input.GetAxis("Horizontal") > 0f))
         {
             anim.SetBool("walk", true);
             transform.eulerAngles = new Vector3(0f, 0f, 0f);
         }
 
-        if (Input.GetAxis("Horizontal") < 0f)
+        if ((Input.GetAxis("Horizontal") < 0f))
         {
             anim.SetBool("walk", true);
             transform.eulerAngles = new Vector3(0f, 180f, 0f);
         }
 
-        if (Input.GetAxis("Horizontal") == 0f)
+        if ((Input.GetAxis("Horizontal") == 0f))
         {
             anim.SetBool("walk", false);
         }
     }
 
     void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
         {
-            if (Input.GetButtonDown("Jump"))
+            if (!isJumping)
             {
-                if (!isJumping)
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                isJumping = true;
+                DoubleJump = true;
+                anim.SetBool("jump", true);
+            }
+            else
+            {
+                if(DoubleJump)
                 {
                     rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
-                    doubleJump = true;
-                    anim.SetBool("jump", true);
-                }
-                else
-                {
-                    if (doubleJump)
-                    {
-                        rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
-                        doubleJump = false;
-                    }
+                    DoubleJump = false;
+                    anim.SetBool("DoubleJump", true);
                 }
             }
-        }
 
-        void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (collision.gameObject.layer == 8)
-            {
-                isJumping = false;
-                anim.SetBool("jump", false);
-            }
-        }
 
-        void OnCollisionExit2D(Collision2D collision)
-        {
-            if (collision.gameObject.layer == 8)
-            {
-                isJumping = true;
-            }
         }
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+            anim.SetBool("jump", false);
+            anim.SetBool("DoubleJump", false);
+            anim.SetBool("Fall", false);
+            anim.SetBool("DoubleJump", false);
+        }
+        
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isJumping = true;
+
+        }
+    }
+}
